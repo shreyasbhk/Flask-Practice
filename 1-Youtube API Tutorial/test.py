8b8202507042f4b538bc498116b8a288033bdea3
Shreyas H <hshreyas15@gmail.com>
2020-08-29 19:37:18 -0700

    Get, Put, & Delete working using a Python dict

--- 1-Youtube API Tutorial/test.py
+++ 1-Youtube API Tutorial/test.py
@@ -8,11 +8,15 @@ data = [{"likes": 645, "views": 10000, "name": "Going to the Forrest with Jake"}
         {"likes": 543, "views": 74989, "name": "Apology from Jake"}]
 
 for i in range(len(data)):
-response = requests.put(BASE + "video/1", {"likes": 10, "views": 10, "name": "Going to the Forrest with Jake"})
-print(response.json())
-print()
-response = requests.get(BASE + "video/1")
+        response = requests.put(BASE + "video/" + str(i), data[i])
+        print(response.json())
+
+response = requests.delete(BASE + "video/0")
 print(response)
+
+response = requests.get(BASE + "video/2")
+print(response.json())
+
 """
 - 3
 response = requests.put(BASE + "video/1", {"likes": 10, "views": 10, "name": "Going to the Forrest with Jake"})
