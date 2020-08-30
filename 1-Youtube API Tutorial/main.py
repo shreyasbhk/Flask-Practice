4f3cc1605c8442a94762a584e907242b10d58fa6
Shreyas H <hshreyas15@gmail.com>
2020-08-29 19:47:35 -0700

    Database Model setup and initialized

--- 1-Youtube API Tutorial/main.py
+++ 1-Youtube API Tutorial/main.py
@@ -3,9 +3,23 @@
 
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+    
+    def __repr__(self):
+        return f"Video(name = {name}, views = {views}, likes = {likes})"
+
+# db.create_all() - Only run once to initialize the database
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
